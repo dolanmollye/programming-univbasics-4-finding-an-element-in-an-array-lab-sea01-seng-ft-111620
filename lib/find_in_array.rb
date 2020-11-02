f73bc74cc8c1d52fb324f88f41fbf81702f9607f
Molly Dolan <dolanmollye@gmail.com>
2020-11-02 18:47:02 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,11 @@
 def find_element_index(array, value_to_find)
   count = 0 
+  find_index = nil
+  while count < array.length do
+    if array[count] == value_to_find
+      find_index = count
+    end
+    count += 1 
+  end
+  find_index
 end
